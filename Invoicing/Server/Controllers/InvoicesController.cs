c540eae9cdee01a73d87fc3e13bf8c5dcce66700
Madezz-o <55086670+Madezz-o@users.noreply.github.com>
2021-01-25 10:21:07 +0100

    Ajoutez des fichiers projet.

---
+++ Invoicing/Server/Controllers/InvoicesController.cs
@@ -0,0 +1,55 @@
+﻿using Invoicing.Shared;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Invoicing.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class InvoicesController : ControllerBase
+    {
+        private readonly ILogger<InvoicesController> _logger;
+        private readonly IBusinessData _data;
+
+        public InvoicesController(ILogger<InvoicesController> logger, IBusinessData data)
+        {
+            _logger = logger;
+            _data = data;
+        }
+
+        [HttpGet]
+        public IEnumerable<Invoice> Get()
+        {
+            return _data.AllInvoices;
+        }
+
+        [HttpGet("{reference}")]
+        public ActionResult<Invoice> Get(string reference)
+        {
+            var invoice = _data.AllInvoices.Where(inv => inv.Reference == reference).FirstOrDefault();
+
+            if(invoice != null)
+            {
+                return invoice;
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPost]
+        public void Post() // Methode exécutée lorsqu'on appui sur le bouton. Il faut envoyer toutes le données rempli par l'utilisateur dans la BDD
+        {
+            
+
+            _data.EnvoyerCA();
+
+            Response.Redirect("/");
+        }
+    }
+}
---
+++ Invoicing/Server/Models/BusinessDataSql.cs
@@ -0,0 +1,83 @@
+﻿using Invoicing.Shared;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using System.Data;
+
+namespace Invoicing.Server.Models
+{
+    public class BusinessDataSql : IBusinessData, IDisposable
+    {
+        private SqlConnection cnct;
+        public BusinessDataSql(string connectionString)
+        {
+            cnct = new SqlConnection(connectionString);
+        }
+
+        public void Dispose()
+        {
+            cnct.Dispose();
+        }
+
+        public IEnumerable<Invoice> AllInvoices
+            => cnct.Query<Invoice>("SELECT Client AS Customer, Numero AS Reference, Datereglement AS Created, MontantDu AS Amount, MontantRegle AS Paid FROM Factures ORDER BY Datereglement DESC");
+
+        
+        //public double ChiffreAffaire => cnct.QuerySingle<double>("SELECT SUM(MontantRegle) FROM Factures");
+
+        public void EnvoyerCA() // On veut faire le calcul du CA puis l'envoyer dans la BDD pour le récupérer plus tard et optimiser le calcul 
+        {
+            double ChiffreAffaire = cnct.QuerySingle<double>("SELECT SUM(MontantRegle) FROM Factures");
+
+            var p = new DynamicParameters();
+            p.Add("@CA", ChiffreAffaire, DbType.Double, ParameterDirection.Input);
+
+            //TEST de créer nouvelles factures avec données entrées par l'utilisateur 
+
+            String Numero = cnct.QueryFirst<String>("SELECT Numero FROM Factures");
+            String Client = cnct.QueryFirst<String>("SELECT Client FROM Factures");
+            DateTime Datereglement = cnct.QueryFirst<DateTime>("SELECT Datereglement FROM Factures");
+            double MontantDu = cnct.QueryFirst<double>("SELECT MontantDu FROM Factures");
+            double MontantRegle = cnct.QueryFirst<double>("SELECT MontantRegle FROM Factures");
+
+
+
+
+            p.Add("@Numero", Numero, DbType.String, System.Data.ParameterDirection.Input);
+            p.Add("@Client", Client, DbType.String, System.Data.ParameterDirection.Input);
+            p.Add("@Datereglement", Datereglement, DbType.DateTime, System.Data.ParameterDirection.Input);
+            p.Add("@MontantDu", MontantDu, DbType.Double, System.Data.ParameterDirection.Input);
+            p.Add("@MontantRegle", MontantRegle, DbType.Double, System.Data.ParameterDirection.Input);
+
+            cnct.Execute(@"INSERT INTO Factures (Numero, Client, Datereglement, MontantDu, MontantRegle, CA) VALUES (@Numero, @Client, @Datereglement, @MontantDu, @MontantRegle, @CA)", p);
+
+        }
+
+
+
+
+        /*
+        public void AjouterFacture(String Numero, String Client, DateTime Datereglement, double MontantDu, double MontantRegle, float CA)
+        {
+            var p = new DynamicParameters();
+            p.Add("@Numero", Numero, DbType.String, System.Data.ParameterDirection.Input);
+            p.Add("@Client", Client, DbType.String, System.Data.ParameterDirection.Input);
+            p.Add("@Datereglement", Datereglement, DbType.DateTime, System.Data.ParameterDirection.Input);
+            p.Add("@MontantDu", MontantDu, DbType.Double, System.Data.ParameterDirection.Input);
+            p.Add("@MontantRegle", MontantRegle, DbType.Double, System.Data.ParameterDirection.Input);
+            p.Add("@CA", ChiffreAffaire + MontantRegle, DbType.Double, ParameterDirection.Input);
+
+            cnct.Execute(@"INSERT INTO Factures (Numero, Client, Datereglement, MontantDu, MontantRegle, CA) VALUES (@Numero, @Client, @Datereglement, @MontantDu, @MontantRegle, @CA)", p);
+        }
+        */
+
+
+        public double SalesRevenue => throw new NotImplementedException();
+
+        public double Outstanding => throw new NotImplementedException();
+
+    }
+}
